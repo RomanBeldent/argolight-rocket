865625eaedae13c9e12c6f6ea32415bdc7c02b23
RomanBeldent <romanbeldent@gmail.com>
2024-04-16 13:16:12 +0200

    wip api, checking out if I'm going to use mongodb or a simple json file to save datas

--- argolight-rocket-api/app.js
+++ argolight-rocket-api/app.js
@@ -1,1 +1,13 @@
-console.log('Hello')
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello, Express !'))
+
+app.get('/api/rockets/:id', (req, res) => {
+    const id = req.params.id
+    res.send(`Voici la requête ${id}`)
+})
+
+app.listen(port, () => console.log(`Notre app node est démarré sur le port : ${port}`))
