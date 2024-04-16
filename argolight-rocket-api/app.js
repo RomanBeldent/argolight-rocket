674ff6aa10fbebb16ed1a3c3cdd807abe42bf8aa
RomanBeldent <romanbeldent@gmail.com>
2024-04-16 15:16:08 +0200

    helper message

--- argolight-rocket-api/app.js
+++ argolight-rocket-api/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { success } = require('./helper')
 let rockets = require('./mock-rocket')
 
 const app = express()
@@ -6,11 +7,15 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello, Express !'))
 
-
 app.get('/api/rockets/:id', (req, res) => {
     const id = parseInt(req.params.id)
     const rocket = rockets.find(rocket => rocket.id === id)
-    res.send(`${rocket.description}`)
+    const message = 'Une fusée a été trouvé.'
+    res.json(success(message, rocket))
 })
 
-app.listen(port, () => console.log(`Notre app node est démarré sur le port : ${port}`))
+app.get('/api/rockets', (req, res) => {
+    res.json(`Il y a un total de ${rockets.length} fusées dans l'API pour l'instant`)
+})
+
+app.listen(port, () => console.log(`Notre app node est démarré sur le port : http://localhost:${port}`))
