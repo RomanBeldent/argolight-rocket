392576f4bc1779dd1d742d1999022b583b8837fe
RomanBeldent <romanbeldent@gmail.com>
2024-04-16 15:03:03 +0200

    mock rocket with datas

--- argolight-rocket-api/app.js
+++ argolight-rocket-api/app.js
@@ -1,13 +1,16 @@
 const express = require('express')
+let rockets = require('./mock-rocket')
 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello, Express !'))
 
+
 app.get('/api/rockets/:id', (req, res) => {
-    const id = req.params.id
-    res.send(`Voici la requête ${id}`)
+    const id = parseInt(req.params.id)
+    const rocket = rockets.find(rocket => rocket.id === id)
+    res.send(`${rocket.description}`)
 })
 
 app.listen(port, () => console.log(`Notre app node est démarré sur le port : ${port}`))
