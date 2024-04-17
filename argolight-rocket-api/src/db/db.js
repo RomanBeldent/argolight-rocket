1a8dcee5ed409d6985d04110fa2bef530c83728d
RomanBeldent <romanbeldent@gmail.com>
2024-04-17 14:10:29 +0200

    atlas ok

--- argolight-rocket-api/src/db/db.js
+++ argolight-rocket-api/src/db/db.js
@@ -1,10 +1,13 @@
 const { MongoClient } = require('mongodb')
 
 let dbConnection
-let uri = 'mongodb+srv://argolight:argolight@cluster0.wdcbpqg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+// Entrez ici le mdp envoyez par mail pour accéder à la DB
+let password = 'mIdV6OTjRlF7Cwct'
+const uri = `mongodb+srv://argolight:${password}@cluster0.wdcbpqg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
 module.exports = {
     connectToDb: (cb) => {
+        //mongodb://localhost:27017/spacex
         MongoClient.connect(uri)
         .then((client) => {
             dbConnection = client.db()
