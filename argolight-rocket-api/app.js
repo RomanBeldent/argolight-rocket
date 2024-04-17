eb01f01b093485a6a19a240689f76d80e94eff66
RomanBeldent <romanbeldent@gmail.com>
2024-04-17 15:20:22 +0200

    delete method added, added success message to post method & success message to delete method

--- argolight-rocket-api/app.js
+++ argolight-rocket-api/app.js
@@ -66,15 +66,33 @@ app.get('/api/rockets/:id', (req, res) => {
 
 app.post('/api/rockets', (req, res) => {
     const rocket = req.body
+    const message = `The rocket ${req.body.name} has been added`
 
     db.collection('rockets')
         .insertOne(rocket)
-        .then(result => {
-            res.status(201).json(result)
+        .then(rocket => {
+            res.status(201).json(success(message, rocket))
         })
         .catch(err => {
             res.status(500).json({ err: 'Could not create a new rocket' })
         })
+})
+
+app.delete('/api/rockets/:id', (req, res) => {
+    const message = `The rocket has been properly deleted`
+
+    if (ObjectId.isValid(req.params.id)) {
+        db.collection('rockets')
+            .deleteOne({ _id: new ObjectId(req.params.id) })
+            .then(rocket => {
+                res.status(200).json(success(message, rocket))
+            })
+            .catch(err => {
+                res.status(500).json({ error: 'Could not fetch the rocket' })
+            })
+    } else {
+        res.status(500).json({ error: 'Not a valid rocket ID' })
+    }
 })
 
 // 404 route error handling
