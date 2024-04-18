f712f4681945265c0c270f328295585516f770b8
RomanBeldent <romanbeldent@gmail.com>
2024-04-18 13:36:31 +0200

    form wip

---
+++ argolight-rocket-front/src/components/LoginView.vue
@@ -0,0 +1,29 @@
+<template>
+    <form name="login-form">
+        <div class="mb-3">
+            <label for="name">Username: </label>
+            <input id="name" type="text" />
+        </div>
+        <div class="mb-3">
+            <label for="password">Password: </label>
+            <input id="password" type="password" />
+        </div>
+        <button class="btn btn-outline-dark" type="submit">
+            Login
+        </button>
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'LoginView',
+    data() {
+        return {
+            input: {
+                name: "",
+                password: ""
+            }
+        }
+    }
+}
+</script>
