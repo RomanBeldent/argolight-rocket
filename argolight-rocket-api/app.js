fc06012cdb04bb928da91eb8feee8b326dd68a6f
RomanBeldent <romanbeldent@gmail.com>
2024-04-22 10:39:35 +0200

    dotenv installed

--- argolight-rocket-api/app.js
+++ argolight-rocket-api/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+require('dotenv').config()
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const { connectToDb, getDb } = require('./src/db/db')
