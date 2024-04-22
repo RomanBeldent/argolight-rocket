524f305afac535791c4e3685c341f85f1ebee8f2
RomanBeldent <romanbeldent@gmail.com>
2024-04-22 15:06:43 +0200

    login page ok

--- argolight-rocket-front/src/App.vue
+++ argolight-rocket-front/src/App.vue
@@ -1,14 +1,23 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <LoginView/>
+  <RouterView />
 </template>
 
 <script>
-import LoginView from './components/LoginView.vue'
 export default {
   name: 'App',
   components: {
-    LoginView
   }
 }
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
